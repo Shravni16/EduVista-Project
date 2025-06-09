4ca172ee8618bbb30e855468db95f73f6e2d9e94
Shravni16 <145959571+Shravni16@users.noreply.github.com>
2025-06-10 00:40:33 +0530

    Update CourseCard.jsx

--- src/components/core/Dashboard/CatalogCourses/CourseCard.jsx
+++ src/components/core/Dashboard/CatalogCourses/CourseCard.jsx
@@ -3,9 +3,11 @@ import GetAvgRating from "../../../../utils/avgRating";
 import RatingStars from "../../../common/RatingStars"
 import "./catlog.css"
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function CourseCard({Course}){
     const [avg,setAvg]=useState(0);
+    const {token}=useSelector((state)=>state.auth);
     useEffect(()=>{
     setAvg(GetAvgRating(Course.ratingAndReview))
 
@@ -14,7 +16,7 @@ function CourseCard({Course}){
 
     <div className="catlog-div-wrap-child">
 
-    <Link to={`/courses/${Course._id}`}> <img className="img-size" src={Course.thumbnail}></img></Link>
+    <Link to={token? `/courses/${Course._id}`:`/not-found`}> <img className="img-size" src={Course.thumbnail}></img></Link>
     <div className="catlog-name-instructor"><p className="catlog-grid-ch-1">{Course.courseName}</p><p className="catlog-grid-ch-2">By {Course?.instructor?.firstName} {Course?.instructor?.lastName}</p></div>
    <div className="catlog-avg-star-rating"><div>{avg}</div> <RatingStars Review_Count={avg}/><p className="catlog-rating-count"> {Course.ratingAndReview.length} Ratings</p></div>
    <div className="catlog-course-price">Rs. {Course.price}</div>
